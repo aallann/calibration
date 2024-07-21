36511df827edbe8d10e7fcadd513e1df8e3f476b
aallann <ucabjo0@ucl.ac.uk>
2024-07-21 19:39:32 +0100

    feat:blackscholes include first iter

---
+++ include/black_scholes.h
@@ -0,0 +1,45 @@
+#ifndef BLACK_SCHOLES_H
+#define BLACK_SCHOLES_H
+
+#include <model.h>
+#include <options.h>
+#include <utils/standard_gaussian.h>
+
+#include <cmath>
+
+class BlackScholes : public Model {
+   public:
+    explicit BlackScholes(double S);
+    ~BlackScholes() override = default;
+
+    array d1(std::vector<EuropeanOption> &options) const;
+
+    array d2(std::vector<EuropeanOption> &options) const;
+
+    std::shared_ptr<array> strikeFromDelta();
+
+    array getDeltaFromStrike();
+
+    array price(std::vector<EuropeanOption> &options);
+
+    array impVol(std::vector<EuropeanOption> &options);
+
+    inline static void setStrikes(std::shared_ptr<array> strikes) {
+        BlackScholes::strikes = strikes;
+    }
+
+    inline static void setPrices(array prices) {
+        BlackScholes::prices = prices;
+    }
+
+    inline static array getPrices(){
+        return BlackScholes::prices;
+    }
+
+   private:
+    inline static StandardGaussian standardGaussian;
+    inline static std::shared_ptr<array> strikes = nullptr;
+    static array prices;
+};
+
+#endif  // BLACK_SCHOLES_H

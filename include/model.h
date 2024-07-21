1538c32d46ac5593dee36e852f39ca098b973ea5
aallann <ucabjo0@ucl.ac.uk>
2024-07-21 14:49:02 +0100

    feat: model

--- include/model.h
+++ include/model.h
@@ -9,12 +9,12 @@ class Model {
    public:
     Model(
         double S,
-        std::shared_ptr<array> vol,
-        std::shared_ptr<array> tau,
-        std::shared_ptr<array> rfb,
-        std::shared_ptr<array> rft,
-        std::shared_ptr<array> deltas,
-        std::shared_ptr<array> omegas
+        std::shared_ptr<array> vol = nullptr,
+        std::shared_ptr<array> tau = nullptr,
+        std::shared_ptr<array> rfb = nullptr,
+        std::shared_ptr<array> rft = nullptr,
+        std::shared_ptr<array> deltas = nullptr,
+        std::shared_ptr<array> omegas = nullptr
     );
 
     virtual ~Model() = default;

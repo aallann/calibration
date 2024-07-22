2bf6f1af92dbb07673c91bc80634988aebc20cfe
aallann <ucabjo0@ucl.ac.uk>
2024-07-23 00:23:33 +0100

    void: test

--- include/black_scholes.h
+++ include/black_scholes.h
@@ -9,19 +9,14 @@
 
 class BlackScholes : public Model {
    public:
-    explicit BlackScholes(double S);
+    using Model::Model;
     ~BlackScholes() override = default;
 
     array d1(std::vector<EuropeanOption> &options) const;
-
     array d2(std::vector<EuropeanOption> &options) const;
-
     std::shared_ptr<array> strikeFromDelta();
-
-    array getDeltaFromStrike();
-
+    array deltaFromStrike();
     array price(std::vector<EuropeanOption> &options);
-
     array impVol(std::vector<EuropeanOption> &options);
 
     inline static void setStrikes(std::shared_ptr<array> strikes) {

40e40faed78185f4038f745d2598a28fe22085dd
aallann <ucabjo0@ucl.ac.uk>
2024-08-07 21:27:16 +0200

    cosmetic, indentation inconsistencies in model base class

--- calibration/model.cc
+++ calibration/model.cc
@@ -2,20 +2,20 @@
 
 
 Model::Model(
-        double S,
-        std::shared_ptr<array> vol,
-        std::shared_ptr<array> tau,
-        std::shared_ptr<array> rfb,
-        std::shared_ptr<array> rft,
-        std::shared_ptr<array> deltas,
-        std::shared_ptr<array> omegas
-    ) : S(S) {
-        Model::vol = vol;
-        Model::tau = tau;
-        Model::rfb = rfb;
-        Model::rft = rft;
-        Model::deltas = deltas;
-        Model::omegas = omegas;
+    double S,
+    std::shared_ptr<array> vol,
+    std::shared_ptr<array> tau,
+    std::shared_ptr<array> rfb,
+    std::shared_ptr<array> rft,
+    std::shared_ptr<array> deltas,
+    std::shared_ptr<array> omegas
+) : S(S) {
+    Model::vol = vol;
+    Model::tau = tau;
+    Model::rfb = rfb;
+    Model::rft = rft;
+    Model::deltas = deltas;
+    Model::omegas = omegas;
 };
 
 void Model::setVol(std::shared_ptr<array> vol) {
